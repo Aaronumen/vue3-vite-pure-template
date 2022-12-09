e22ab1546a1101917b66859bb137b597e4ebced2
Aaronumen <65692618+Aaronumen@users.noreply.github.com>
2022-12-09 23:52:43 +0800

    ✨ feat: init

---
+++ vite.config.ts
@@ -0,0 +1,93 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import AutoImport from "unplugin-auto-import/vite"
+import path from "path"
+import commpressPlugin from "vite-plugin-compression"
+import { visualizer } from "rollup-plugin-visualizer"
+import vueJsx from "@vitejs/plugin-vue-jsx"
+import autoprefixer from 'autoprefixer'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx({}),
+    AutoImport({
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+      imports: [
+        // 插件预设支持导入的api
+        "vue",
+        "vue-router",
+        // 自定义导入的api
+      ],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+      dts: "./auto-imports.d.ts"
+    }),
+    commpressPlugin({
+      verbose: true, // 默认即可
+      disable: false, //开启压缩(不禁用)，默认即可
+      deleteOriginFile: false, //删除源文件
+      threshold: 10240, //压缩前最小文件大小
+      algorithm: "gzip", //压缩算法
+      ext: ".gz" //文件类型
+    }),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: "buildSize.html", //分析图生成的文件名
+      open: false //如果存在本地服务端口，将在打包后自动展示
+    })
+  ],
+  css: {
+    postcss:{
+      plugins:[
+        autoprefixer({
+          overrideBrowserslist: [
+            "Android 4.1",
+            "iOS 7.1",
+            "Chrome > 31",
+            "ff > 31",
+            "ie >= 8"
+            //'last 10 versions', // 所有主流浏览器最近2个版本
+        ],
+        grid: true
+      })]
+    },
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          hack: `true; @import (reference) "${path.resolve('src/styles/global.less')}";`,
+        },
+      },
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    }
+  },
+  build: {
+    assetsInlineLimit: 4096,
+    // sourcemap:true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+        manualChunks(id) {
+          if (id.includes("node_modules/.pnpm")) {
+            return id
+              .toString()
+              .split("node_modules/.pnpm/")[1]
+              .split("/")[0]
+              .toString()
+          }
+        }
+      }
+    }
+  }
+})
